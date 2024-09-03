42228ee42ba8dd4392df3693dae2e7acb59c08cc
Jeff <he.man@skele.to>
2024-09-03 13:09:26 -0400

    Inital svelte and tailwind setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1 class="text-3xl font-bold">
+  Amy and Jeff's Wedding Site
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    color: theme(colors.stone.300);
+    background-color: theme(colors.stone.950)
+  }
+</style>
