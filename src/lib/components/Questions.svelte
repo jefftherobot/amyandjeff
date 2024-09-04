10733d558bcaa8f73d58aa9b173cf984dc4b0310
Jeff <he.man@skele.to>
2024-09-04 15:31:58 -0400

    add some content

--- src/lib/components/Questions.svelte
+++ src/lib/components/Questions.svelte
@@ -1,4 +1,38 @@
+<script lang="ts">
+  import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+</script>
 
 <h2 class="h2">Q&A</h2>
 
+<div class="p-4 md:p-7 space-y-4 bg-gradient-to-br variant-gradient-primary-secondary">
+  <Accordion autocollapse hover="hover:bg-tertiary-hover-token" padding="py-4 px-4" class="bg-surface-100-800-token">
+    <AccordionItem open>
+      <svelte:fragment slot="lead"><i class="fa-solid fa-paragraph text-xl w-6 text-center"></i></svelte:fragment>
+      <svelte:fragment slot="summary">
+        <p class="font-bold">
+          Are children allowed?
+        </p>
+      </svelte:fragment>
+      <svelte:fragment slot="content">
+        <p>
+          Sorry, no
+        </p>
+      </svelte:fragment>
+    </AccordionItem>
+    <AccordionItem>
+      <svelte:fragment slot="lead"><i class="fa-solid fa-paragraph text-xl w-6 text-center"></i></svelte:fragment>
+      <svelte:fragment slot="summary">
+        <p class="font-bold">
+          Is there a hotel reservation block?
+        </p>
+      </svelte:fragment>
+      <svelte:fragment slot="content">
+        <p>
+          Yes see here
+        </p>
+      </svelte:fragment>
+    </AccordionItem>
+    <!-- ... -->
+  </Accordion>
+</div>
 
--- src/lib/components/Venue.svelte
+++ src/lib/components/Venue.svelte
@@ -1,4 +1,4 @@
 
 <h2 class="h2">The Old Mill</h2>
 
-display google map as background with address overlay
+<iframe width="600" height="450" style="border:0; filter: grayscale(100%) invert(92%)" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJjXSog_joxokRg8Tl6xKN9Ow&key=AIzaSyCe8pbI7wgSmHE69F-0gQWBQVyG5GGMYOI"></iframe> 
