5a8e45a904a4590ff6713a2bc2fefcce41d54f25
Jeff <he.man@skele.to>
2025-04-15 20:45:29 -0400

    updates to form

--- src/lib/components/Registry.svelte
+++ src/lib/components/Registry.svelte
@@ -1,12 +1,13 @@
+<div class="p-10 md:p-15 space-y-8 "> 
+  <h2 class="h2 mb-10">Registry</h2>
 
-<h2 class="h2 mb-10">Registry</h2>
+  <p>info about registry options</p>
 
-<p>info about registry options</p>
-
-<div class="flex justify-center items-center mx-auto transition-[width] duration-200 w-full ">
-  <div class="gap-4">
-    <a href="#" class="btn variant-filled-tertiary">Option 1</a>
-    <a href="#" class="btn variant-filled-secondary">Option 2</a>
+  <div class="flex justify-center items-center mx-auto transition-[width] duration-200 w-full ">
+    <div class="gap-4">
+      <a href="#" class="btn variant-filled-tertiary">Option 1</a>
+      <a href="#" class="btn variant-filled-secondary">Option 2</a>
+    </div>
   </div>
 </div>
 
--- src/lib/components/Rsvp.svelte
+++ src/lib/components/Rsvp.svelte
@@ -36,48 +36,58 @@
 
 <div class="p-4 md:p-10 space-y-4 bg-gradient-to-br variant-gradient-primary-secondary "> 
   <h2 class="h2 mb-10">RSVP</h2>
-  {#if submitted}
-    <p>Thank you for your RSVP!</p>
-  {:else}
-  <form on:submit|preventDefault={submitForm}>
-    <label>
-      Name:
-      <input type="text" bind:value={name} required />
-    </label>
+  <div class="mx-auto max-w-screen-md bg-surface-100-800-token ">
+    <div class="card p-4 w-full text-token space-y-4">
+      {#if submitted}
+        <p>Thank you for your RSVP!</p>
+      {:else}
+      <form on:submit|preventDefault={submitForm}>
 
-    <label>
-      Email:
-      <input type="email" bind:value={email} required />
-    </label>
-    <label>
-      Guest:
-      <input type="number" bind:value={guest} required />
-    </label>
-    <label>
-      Diet Restrictions:
-      <input type="text" bind:value={diet} required />
-    </label>
 
-    <label>
-      Will you attend?
-      <select bind:value={attending}>
-        <option value="Yes">Yes</option>
-        <option value="No">No</option>
-      </select>
-    </label>
+        <div class="w-full grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+          <div class="input-group-shim"><i class="fa-solid fa-envelope"></i></div>
+          <input type="email" placeholder="Your email..." bind:value={email} required />
+        </div>
 
-    <label>
-      Invite Code:
-      <input type="text" bind:value={inviteCode} required />
-    </label>
 
-    <button type="submit">Submit RSVP</button>
+        <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+          <div class="input-group-shim"><i class="fa-solid fa-user"></i></div>
+          <input type="text" placeholder="Your first and last name..." bind:value={name} required />
+        </div>
 
-    {#if error}
-      <p style="color:red;">{error}</p>
+        <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+          <div class="input-group-shim"><i class="fa-solid fa-users"></i></div>
+          <input type="number" placeholder="Your guest count" bind:value={guest} required />
+        </div>
+
+        <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+          <div class="input-group-shim"><i class="fa-solid fa-utensils"></i></div>
+          <input type="text" placeholder="Any Dietary Restrictions?" bind:value={diet} required />
+        </div>
+
+        <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+          <div class="input-group-shim"><i class="fa-solid fa-envelope-open-text"></i></div>
+          <select class="select" bind:value={attending} required>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+        </div>
+
+        <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+          <div class="input-group-shim"><i class="fa-solid fa-key"></i></div>
+          <input class="input" type="text" placeholder="Your invite code..." bind:value={inviteCode} required />
+        </div>
+      </div>
+        <button type="submit" class="btn variant-filled-primary mt-5">Submit RSVP</button>
+
+        {#if error}
+          <p style="color:red;">{error}</p>
+        {/if}
+      </form>
     {/if}
-  </form>
-{/if}
+    </div>
+  </div>
 </div>
 <!-- <div class="p-4 md:p-10 space-y-4 bg-gradient-to-br variant-gradient-primary-secondary "> 
   <h2 class="h2 mb-10">RSVP</h2>
